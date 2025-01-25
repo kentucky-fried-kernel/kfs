5de62c592822404a5747fa7282a2036272c13f06
Arthur Bied-Charreton <136271426+winstonallo@users.noreply.github.com>
2025-01-25 21:27:58 +0100

    fix: typo in src/panic.rs

--- src/panic.rs
+++ src/panic.rs
@@ -9,7 +9,7 @@ fn panic(_info: &PanicInfo) -> ! {
     use crate::terminal::{vga::Buffer, Terminal};
 
     let mut t = Terminal::default();
-    t.write_color_str("Paniced!", Color::Error as u8);
+    t.write_color_str("Panicked!", Color::Error as u8);
     let b = Buffer::from_screen(t.active_screen());
     b.flush();
     loop {}
