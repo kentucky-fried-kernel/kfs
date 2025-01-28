98204faaf63496f641a0f4716bd682192861a10b
Felix Brüggemann <57171559+FelixBrgm@users.noreply.github.com>
2025-01-28 19:07:34 +0000

    format fixes

--- src/panic.rs
+++ src/panic.rs
@@ -6,7 +6,7 @@ use crate::terminal::vga::Color;
 #[cfg(not(test))]
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
-    use crate::terminal::{vga::Buffer, Screen };
+    use crate::terminal::{vga::Buffer, Screen};
 
     let mut s = Screen::default();
     s.write_color_str("Panicked!", Color::Error as u8);
