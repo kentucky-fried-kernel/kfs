6054eb063b95f9cc58476ff9387e31d71148ce9b
Arthur Bied-Charreton <arthurbiedchar@gmail.com>
2025-03-02 07:37:28 +0000

    refactor: move ps/2 io operations to separate file

--- src/ps2/mod.rs
+++ src/ps2/mod.rs
@@ -1,5 +1,12 @@
 mod controller;
+mod io;
 mod scancodes;
 
-pub use controller::{init, read_if_ready};
+pub const DATA_PORT: u16 = 0x60;
+pub const STATUS_PORT: u16 = 0x64;
+pub const COMMAND_PORT: u16 = 0x64;
+pub const OUTPUT_BUFFER_STATUS_BIT: u8 = 1;
+
+pub use controller::init;
+pub use io::read_if_ready;
 pub use scancodes::Key;
