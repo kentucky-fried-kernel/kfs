e64ba76b927f3af96ccac23dd26619f509694440
Arthur Bied-Charreton <arthurbiedchar@gmail.com>
2025-03-02 09:28:30 +0000

    fix: change u64_to_base to u32_to_base

--- src/print.rs
+++ src/print.rs
@@ -13,12 +13,12 @@ pub fn slice_to_str((slice, len): (&[u8; 65], usize)) -> Result<&str, ParseError
     }
 }
 #[allow(unused)]
-pub fn u64_to_base(mut addr: u64, base: u8) -> Result<([u8; 65], usize), ()> {
+pub fn u32_to_base(mut addr: u32, base: u8) -> Result<([u8; 32], usize), ()> {
     if !(2..=16).contains(&base) {
         return Err(());
     }
 
-    let mut buf: [u8; 65] = [0; 65];
+    let mut buf: [u8; 32] = [0; 32];
     let digits: &[u8; 16] = b"0123456789ABCDEF";
 
     if addr == 0 {
@@ -30,8 +30,8 @@ pub fn u64_to_base(mut addr: u64, base: u8) -> Result<([u8; 65], usize), ()> {
 
     while addr != 0 && idx > 0 {
         idx -= 1;
-        buf[idx] = digits[(addr % base as u64) as usize];
-        addr /= base as u64;
+        buf[idx] = digits[(addr % base as u32) as usize];
+        addr /= base as u32;
     }
 
     if addr != 0 {
@@ -44,14 +44,14 @@ pub fn u64_to_base(mut addr: u64, base: u8) -> Result<([u8; 65], usize), ()> {
 }
 
 #[cfg(test)]
-mod u64_to_base_test {
+mod u32_to_base_test {
     use super::*;
 
     #[test]
     fn test_normal_functionality_base_16_ff() {
         let num = 255u64;
 
-        let res = match u64_to_base(num, 16) {
+        let res = match u32_to_base(num, 16) {
             Ok((len, buf)) => (len, buf),
             _ => ([0u8; 65], 0),
         };
@@ -67,7 +67,7 @@ mod u64_to_base_test {
     fn test_normal_functionality_base_16_ffff() {
         let num = 65535u64;
 
-        let res = match u64_to_base(num, 16) {
+        let res = match u32_to_base(num, 16) {
             Ok((len, buf)) => (len, buf),
             _ => ([0u8; 65], 0),
         };
@@ -83,7 +83,7 @@ mod u64_to_base_test {
     fn test_normal_functionality_base_16_ffffff() {
         let num = 16777215u64;
 
-        let res = match u64_to_base(num, 16) {
+        let res = match u32_to_base(num, 16) {
             Ok((len, buf)) => (len, buf),
             _ => ([0u8; 65], 0),
         };
@@ -99,7 +99,7 @@ mod u64_to_base_test {
     fn test_normal_functionality_base_16_ffffffff() {
         let num = 4294967295u64;
 
-        let res = match u64_to_base(num, 16) {
+        let res = match u32_to_base(num, 16) {
             Ok((len, buf)) => (len, buf),
             _ => ([0u8; 65], 0),
         };
