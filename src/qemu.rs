9de57d0d3bc0518a5f84970f00a7fcea1320ded5
winstonallo <arthurbiedchar@gmail.com>
2025-10-05 14:17:23 +0200

    Clean up qemu.rs

--- src/qemu.rs
+++ src/qemu.rs
@@ -3,12 +3,12 @@ use crate::port::Port;
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(u32)]
 #[allow(unused)]
-pub enum QemuExitCode {
+pub enum ExitCode {
     Success = 0x10,
     Failed = 0x11,
 }
 
-pub unsafe fn exit_qemu(exit_code: QemuExitCode) {
+pub unsafe fn exit(exit_code: ExitCode) {
     unsafe {
         let port = Port::new(0xf4);
         port.write(exit_code as u8);
