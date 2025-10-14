e83b68bfe2767df078a9e4fc5a10c1e8752d9a61
winstonallo <arthurbiedchar@gmail.com>
2025-10-14 12:57:10 +0200

    Rename run.sh to run-tests.sh

--- run-tests.sh
+++ run-tests.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env sh
 
 mkdir -p build/iso/boot/grub
 i386-elf-as --32 -o build/boot.o src/arch/x86/boot.s
@@ -10,3 +10,12 @@ cp target/i386-unknown-none/release/deps/kfs-421f178bfde42069 build/iso/boot/ker
 grub-mkrescue -v -o build/kernel.iso build/iso --locale-directory=/dev/null --fonts=ascii
 
 qemu-system-i386 -cdrom build/kernel.iso -boot d -device isa-debug-exit,iobase=0xf4,iosize=0x04 -serial stdio -display none
+
+if [ $? -eq 33 ]
+then
+	echo "All tests passed"
+	exit 0
+else
+	echo "Some tests failed"
+	exit 1
+fi
