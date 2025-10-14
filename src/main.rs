fded970011403cfbfa0a1d31b26f0af99d422f19
winstonallo <arthurbiedchar@gmail.com>
2025-10-14 10:55:48 +0200

    Add test runner and test main reexport to main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use terminal::SCREEN;
 
@@ -13,6 +16,15 @@ mod qemu;
 mod shell;
 mod terminal;
 
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    unsafe { printk!("Running {} tests", tests.len()) };
+    for test in tests {
+        test();
+    }
+}
+
+#[cfg(not(test))]
 #[unsafe(no_mangle)]
 pub extern "C" fn kernel_main() {
     if let Err(e) = ps2::init() {
@@ -23,4 +35,21 @@ pub extern "C" fn kernel_main() {
     arch::x86::set_idt();
     #[allow(static_mut_refs)]
     shell::launch(unsafe { &mut SCREEN });
+}
+
+#[cfg(test)]
+#[unsafe(no_mangle)]
+pub extern "C" fn kernel_main() {
+    test_main();
+    unsafe {
+        qemu::exit(qemu::ExitCode::Success);
+    }
+    // if let Err(e) = ps2::init() {
+    //     panic!("could not initialize PS/2: {}", e);
+    // }
+    // arch::x86::gdt::init();
+    // #[cfg(not(test))]
+    // arch::x86::set_idt();
+    // #[allow(static_mut_refs)]
+    // shell::launch(unsafe { &mut SCREEN });
 }
