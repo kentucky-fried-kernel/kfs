fa1a9e51ecfdc47970041a28974f71bad0684f71
winstonallo <arthurbiedchar@gmail.com>
2025-10-16 09:50:14 +0200

    Fix testbuild.py to properly parse the `cargo build` output

--- testbuild.py
+++ testbuild.py
@@ -1,18 +1,33 @@
+# Builds the tests, parses Cargo's JSON output, and writes the absolute path to the
+# test executable to stdout.
+# Example Usage:
+# export TEST_EXECUTABLE_PATH=$(python3 testbuild.py)
 import subprocess
 import sys
+import json
 
 proc = subprocess.run(
-    ["cargo", "build", "--tests", "--release", "--message-format=json"],
+    ["cargo", "build", "--tests", "--release", "--lib", "--message-format=json"],
     capture_output=True,
 )
 
 if proc.returncode != 0:
-    print(str(proc.stdout), file=sys.stderr)
-    print(str(proc.stderr), file=sys.stderr)
+    print("Build failed", file=sys.stderr)
+    print(proc.stdout.decode(), file=sys.stderr)
+    print(proc.stderr.decode(), file=sys.stderr)
     sys.exit(1)
 
-stdout = str(proc.stdout)
-haystack = "target/i386-unknown-none/release/deps/kfs-"
-index = stdout.find(haystack)
-path = stdout[index : index + len(haystack) + 16]
-print(path)
+outputs = [
+    json.loads(output) for output in proc.stdout.decode().split("\n") if output != ""
+]
+
+for output in outputs:
+    if not (executable := output.get("executable")):
+        continue
+    if "target/i386-unknown-none/release/deps/kfs-" not in executable:
+        continue
+    if output["target"]["kind"][0] != "lib":
+        continue
+
+    print(executable)
+    break
