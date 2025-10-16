ff87eaa455876452d89427f371c7b33ecea4b22d
winstonallo <arthurbiedchar@gmail.com>
2025-10-16 08:44:03 +0200

    Print stdout/stderr on build failure in testbuild.py

--- testbuild.py
+++ testbuild.py
@@ -1,9 +1,15 @@
 import subprocess
+import sys
 
 proc = subprocess.run(
     ["cargo", "build", "--tests", "--release", "--message-format=json"],
     capture_output=True,
 )
+
+if proc.returncode != 0:
+    print(str(proc.stdout), file=sys.stderr)
+    print(str(proc.stderr), file=sys.stderr)
+    sys.exit(1)
 
 stdout = str(proc.stdout)
 haystack = "target/i386-unknown-none/release/deps/kfs-"
