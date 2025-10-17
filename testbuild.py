503442fd34bf7e79daa1a1061b1c14af2976b9e1
winstonallo <arthurbiedchar@gmail.com>
2025-10-17 17:14:48 +0200

    Add start of test discovery functionality to testbuild.py

--- testbuild.py
+++ testbuild.py
@@ -1,33 +1,69 @@
-# Builds the tests, parses Cargo's JSON output, and writes the absolute path to the
-# test executable to stdout.
-# Example Usage:
-# export TEST_EXECUTABLE_PATH=$(python3 testbuild.py)
+from pathlib import Path
 import subprocess
 import sys
 import json
+import argparse
+import typing
 
-proc = subprocess.run(
-    ["cargo", "build", "--tests", "--release", "--lib", "--message-format=json"],
-    capture_output=True,
-)
 
-if proc.returncode != 0:
-    print("Build failed", file=sys.stderr)
-    print(proc.stdout.decode(), file=sys.stderr)
-    print(proc.stderr.decode(), file=sys.stderr)
-    sys.exit(1)
+def parse_args(argv: list[str]) -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        description="Utility to build and run the kernel test suite",
+    )
+    parser.add_argument("--unit-tests", action="store_true")
+    parser.add_argument("--end-to-end-tests", action="store_true")
+    return parser.parse_args(argv[1:])
 
-outputs = [
-    json.loads(output) for output in proc.stdout.decode().split("\n") if output != ""
-]
 
-for output in outputs:
-    if not (executable := output.get("executable")):
-        continue
-    if "target/i386-unknown-none/release/deps/kfs-" not in executable:
-        continue
-    if output["target"]["kind"][0] != "lib":
-        continue
+def build_tests() -> str:
+    """Builds tests, returns stdout on success, throws on failure"""
+    proc = subprocess.run(
+        ["cargo", "build", "--tests", "--release", "--lib", "--message-format=json"],
+        capture_output=True,
+    )
+    if proc.returncode != 0:
+        raise RuntimeError(
+            f"Could not build tests:\nstdout:\n{proc.stdout.decode()}\nstderr:\n{proc.stderr.decode()}"
+        )
+    return proc.stdout.decode()
 
-    print(executable)
-    break
+
+def get_unit_tests_executable_path(build_output: str):
+    parsed_output: list[dict[str, typing.Union[str, typing.Any]]] = [
+        json.loads(o) for o in build_output.split("\n") if o != ""
+    ]
+    for o in parsed_output:
+        if not (exe := o.get("executable")):
+            continue
+        if "target/i386-unknown-none/release/deps/kfs-" not in exe:
+            continue
+        # Not quite sure how reliable this is, we will see if this leads to issues
+        # in the future.
+        if o["target"]["kind"][0] != "bin":
+            continue
+
+        return exe
+
+
+def run_unit_tests():
+    build_output = build_tests()
+    test_executable = get_unit_tests_executable_path(build_output)
+
+
+def discover_e2e_tests():
+    test_names = {str(p.name).strip(".rs") for p in Path("./tests").iterdir()}
+    return test_names
+
+
+def run_end_to_end_tests():
+    test_paths = discover_e2e_tests()
+    print(test_paths)
+
+
+if __name__ == "__main__":
+    args = parse_args(sys.argv)
+    if args.unit_tests:
+        run_unit_tests()
+    else:
+        run_end_to_end_tests()
+        ...
