08b908d576abe70a4a2921b6e62e24a6e64b7c4e
winstonallo <arthurbiedchar@gmail.com>
2025-10-21 12:32:06 +0200

    Move multiboot logic into src/boot.rs

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,7 @@
 use core::panic::PanicInfo;
 
 pub mod arch;
+pub mod boot;
 pub mod conv;
 pub mod macros;
 pub mod port;
@@ -18,54 +19,6 @@ pub mod serial;
 pub mod shell;
 pub mod terminal;
 pub mod tester;
-
-const STACK_SIZE: usize = 2 << 20;
-
-#[used]
-#[unsafe(no_mangle)]
-#[unsafe(link_section = ".bss")]
-pub static mut STACK: Stack = Stack([0; STACK_SIZE]);
-
-#[allow(unused)]
-#[repr(align(4096))]
-pub struct Stack([u8; STACK_SIZE]);
-
-#[allow(unused)]
-#[repr(align(4))]
-struct MultibootHeader {
-    magic: usize,
-    flags: usize,
-    checksum: usize,
-}
-
-#[used]
-#[unsafe(no_mangle)]
-#[unsafe(link_section = ".multiboot")]
-static MULTIBOOT_HEADER: MultibootHeader = MultibootHeader {
-    magic: 0x1badb002,
-    flags: 0,
-    checksum: (0usize.wrapping_sub(0x1badb002)),
-};
-
-/// # Safety
-/// This function marks the entrypoint of the kernel executable.
-#[unsafe(naked)]
-#[unsafe(no_mangle)]
-#[unsafe(link_section = ".boot")]
-pub unsafe extern "C" fn _start() {
-    core::arch::naked_asm!(
-        "mov esp, offset STACK + {stack_size}",
-        "push eax",
-        "push ebx",
-        "cli",
-        "call kernel_main",
-        "hang:",
-        "cli",
-        "hlt",
-        "jmp hang",
-        stack_size = const STACK_SIZE
-    )
-}
 
 #[cfg(test)]
 #[unsafe(no_mangle)]
