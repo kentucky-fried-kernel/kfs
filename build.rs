d58c46a4e4e7a374032ce12752f923b4639ecdeb
winstonallo <arthurbiedchar@gmail.com>
2025-11-21 17:35:50 +0100

    Add overengineering to build.rs

--- build.rs
+++ build.rs
@@ -1,4 +1,6 @@
 fn main() {
-    println!("cargo:rerun-if-changed=src/arch/x86/linker.ld");
-    println!("cargo:rerun-if-changed=src/arch/x86/i386-unknown-none.json");
+    let dependencies = ["src/arch/x86/linker.ld", "src/arch/x86/i386-unknown-none.json"];
+    for dep in dependencies {
+        println!("cargo:rerun-if-changed={dep}");
+    }
 }
