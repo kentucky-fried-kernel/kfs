52e3b7e396b97b2a8bcf167e4183c487bfa65103
fbruggem <fbruggem@proton.me>
2025-11-28 17:37:19 +0100

    added new organization

---
+++ src/vmm/paging/init.rs
@@ -0,0 +1,78 @@
+use crate::{
+    boot::KERNEL_BASE,
+    vmm::paging::{
+        Access, PAGE_SIZE,
+        page_entries::{PageDirectoryEntry, PageTableEntry},
+        state::{KERNEL_PAGE_DIRECTORY_TABLE, KERNEL_PAGE_TABLES, PAGE_TABLE_SIZE, USED_PAGES},
+    },
+};
+
+unsafe extern "C" {
+    #[link_name = "_kernel_end"]
+    static KERNEL_END: u8;
+}
+
+fn invalidate(vaddr: usize) {
+    unsafe { core::arch::asm!("invlpg [{}]", in(reg) vaddr) };
+}
+
+#[allow(static_mut_refs)]
+pub fn init_memory(_mem_high: usize, _physical_alloc_start: usize) {
+    let kernel_end = unsafe { &KERNEL_END as *const _ } as usize;
+    let kernel_pages_needed = ((kernel_end + 1) - KERNEL_BASE) / PAGE_SIZE;
+
+    for i in 0..kernel_pages_needed {
+        unsafe {
+            USED_PAGES[i] = Some(Access::Root);
+        }
+
+        let dir_index = i / PAGE_TABLE_SIZE;
+        let page_index = i % PAGE_TABLE_SIZE;
+        let mut e = PageTableEntry::empty();
+        e.set_address(i as u32);
+        e.set_read_write(1);
+        e.set_present(1);
+
+        unsafe {
+            KERNEL_PAGE_TABLES[768 + dir_index].0[page_index] = e;
+        }
+    }
+
+    let mut kernel_page_entries_physical_address = &raw const KERNEL_PAGE_TABLES as usize;
+    kernel_page_entries_physical_address -= KERNEL_BASE;
+
+    for i in 0..=(kernel_pages_needed / PAGE_TABLE_SIZE) {
+        let mut e = PageDirectoryEntry::empty();
+        e.set_address((kernel_page_entries_physical_address / PAGE_SIZE) as u32 + i as u32 + 768);
+        e.set_read_write(1);
+        e.set_present(1);
+
+        unsafe {
+            KERNEL_PAGE_DIRECTORY_TABLE.0[768 + i] = e;
+        }
+    }
+
+    unsafe { KERNEL_PAGE_DIRECTORY_TABLE.0[0] = PageDirectoryEntry::empty() }
+
+    unsafe {
+        for  (i, entry)   in KERNEL_PAGE_DIRECTORY_TABLE.0.iter_mut().enumerate() {
+            let already_set = entry.address() != 0;
+            if already_set {
+                continue;
+            }
+            let mut e = PageDirectoryEntry::empty();
+            e.set_address((kernel_page_entries_physical_address / PAGE_SIZE) as u32 + i as u32);
+            e.set_read_write(1);
+            e.set_present(1);
+
+            *entry = e;
+        }
+    }
+    // Set all page direcotry entries to empty but connect them to the page tables
+
+    invalidate(0);
+
+    for i in 0..=kernel_pages_needed {
+        invalidate(KERNEL_BASE + i * PAGE_SIZE);
+    }
+}
