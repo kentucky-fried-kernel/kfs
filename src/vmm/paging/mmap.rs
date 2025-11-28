1215b71037075ca1b3392c13c561aad1b8c38451
fbruggem <fbruggem@proton.me>
2025-11-28 17:37:19 +0100

    added new organization

---
+++ src/vmm/paging/mmap.rs
@@ -0,0 +1,102 @@
+use crate::vmm::paging::{Access, PAGE_SIZE, Permissions, page_entries::PageTableEntry, state::{KERNEL_PAGE_TABLES, USED_PAGES}};
+
+#[derive(Debug)]
+pub enum MmapError {
+    VaddrRangeAlreadyMapped,
+    NotEnoughMemory,
+    NotImplemented,
+}
+
+#[allow(static_mut_refs)]
+pub fn mmap(vaddr: Option<usize>, size: usize, permissions: Permissions, access: Access) -> Result<usize, MmapError> {
+    if let Some(_) = vaddr {
+        unimplemented!();
+    }
+
+    let pages_needed = (PAGE_SIZE + size - 1) / PAGE_SIZE;
+
+    unsafe {
+        let page_physical_used_lowest_index_user = match USED_PAGES.iter().rev().enumerate().find(|(i, p)| p.is_some_and(|p| p == Access::User)) {
+            Some((i, _)) => i,
+            None => u32::MAX as usize / PAGE_SIZE,
+        };
+        let page_physical_used_highest_index_root = match USED_PAGES.iter().enumerate().find(|(i, p)| p.is_some_and(|p| p == Access::Root)) {
+            Some((i, _)) => i,
+            None => 0,
+        };
+
+        match access {
+            Access::Root => {
+                unimplemented!()
+            }
+            Access::User => {
+                let free_pages_physical_iter = USED_PAGES
+                    .iter()
+                    .enumerate()
+                    .rev()
+                    .take(USED_PAGES.len() - page_physical_used_highest_index_root)
+                    .filter(|(_, b)| b.is_none())
+                    .take(pages_needed);
+
+                let free_pages = free_pages_physical_iter.count();
+                if free_pages < pages_needed {
+                    return Err(MmapError::NotImplemented);
+                }
+
+                let free_pages_physical_iter = USED_PAGES
+                    .iter_mut()
+                    .enumerate()
+                    .rev()
+                    .take(USED_PAGES.len() - page_physical_used_highest_index_root)
+                    .filter(|(_, b)| b.is_none())
+                    .take(pages_needed);
+                let pages_physical_to_be_allocated = free_pages_physical_iter.take(pages_needed);
+
+                let pages_virtual_free_iter = KERNEL_PAGE_TABLES.iter_mut().flat_map(|p| &mut p.0).enumerate();
+                let pages_virtual_to_be_allocated = {
+                    let mut res = None;
+                    for (i, p) in pages_virtual_free_iter {
+                        let all_are_free = pages_needed
+                            == KERNEL_PAGE_TABLES
+                                .iter_mut()
+                                .flat_map(|p| &mut p.0)
+                                .skip(i)
+                                .take(pages_needed)
+                                .filter(|p| p.present() == 0)
+                                .count();
+                        if all_are_free {
+                            res = Some(KERNEL_PAGE_TABLES.iter_mut().flat_map(|p| &mut p.0).enumerate().skip(i));
+                            break;
+                        }
+                    }
+                    res
+                };
+
+                let pages_virtual_to_be_allocated = match pages_virtual_to_be_allocated {
+                    Some(it) => it,
+                    None => return Err(MmapError::VaddrRangeAlreadyMapped),
+                };
+
+                let pages_to_be_allocated = pages_physical_to_be_allocated.zip(pages_virtual_to_be_allocated);
+
+                let mut return_value = Err(MmapError::NotEnoughMemory);
+                for ((physical_i, physical_page), (virtual_i, virtual_page)) in pages_to_be_allocated {
+
+                    *physical_page = Some(access);
+
+                    let mut e = PageTableEntry::empty();
+                    e.set_address(physical_i as u32);
+                    e.set_read_write(permissions as u8);
+                    e.set_present(1);
+                    *virtual_page = e;
+
+                    if let Err(_) = return_value {
+                        return_value = Ok(virtual_i * PAGE_SIZE);
+                    }
+                }
+
+                return return_value;
+            }
+        }
+    }
+}
---
+++ src/vmm/paging/state.rs
@@ -0,0 +1,43 @@
+use crate::vmm::{
+    MEMORY_MAX, paging::{
+        Access,
+        page_entries::{PageDirectory, PageDirectoryEntry, PageTable, PageTableEntry},
+    }
+};
+
+pub const USED_PAGES_SIZE: usize = MEMORY_MAX / super::PAGE_SIZE;
+#[used]
+#[unsafe(no_mangle)]
+#[allow(clippy::identity_op)]
+#[unsafe(link_section = ".data")]
+pub static mut USED_PAGES: [Option<Access>; USED_PAGES_SIZE] = [None; USED_PAGES_SIZE];
+
+pub const PAGE_TABLE_SIZE: usize = 1024;
+#[used]
+#[unsafe(no_mangle)]
+#[allow(clippy::identity_op)]
+#[unsafe(link_section = ".data")]
+pub static mut KERNEL_PAGE_TABLES: [PageTable; PAGE_TABLE_SIZE] = [PageTable([PageTableEntry::empty(); PAGE_TABLE_SIZE]); PAGE_TABLE_SIZE];
+
+pub const KERNEL_PAGE_DIRECTORY_TABLE_SIZE: usize = 1024;
+#[used]
+#[unsafe(no_mangle)]
+#[allow(clippy::identity_op)]
+#[unsafe(link_section = ".data")]
+pub static mut KERNEL_PAGE_DIRECTORY_TABLE: PageDirectory = {
+
+    let mut dir: [PageDirectoryEntry; KERNEL_PAGE_DIRECTORY_TABLE_SIZE] = [PageDirectoryEntry::from_usize(0); KERNEL_PAGE_DIRECTORY_TABLE_SIZE];
+
+    dir[0] = PageDirectoryEntry::from_usize((0 << 22) | 0b10000011);
+    dir[1] = PageDirectoryEntry::from_usize((1 << 22) | 0b10000011);
+    dir[2] = PageDirectoryEntry::from_usize((2 << 22) | 0b10000011);
+    dir[3] = PageDirectoryEntry::from_usize((3 << 22) | 0b10000011);
+
+    // Sets mappings temporary so that the kernel is mapped to the upper half of the vm space
+    dir[768] = PageDirectoryEntry::from_usize((0 << 22) | 0b10000011);
+    dir[769] = PageDirectoryEntry::from_usize((1 << 22) | 0b10000011);
+    dir[770] = PageDirectoryEntry::from_usize((2 << 22) | 0b10000011);
+    dir[771] = PageDirectoryEntry::from_usize((3 << 22) | 0b10000011);
+
+    PageDirectory(dir)
+};
