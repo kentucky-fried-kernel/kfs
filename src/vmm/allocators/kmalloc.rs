40a82970c3e53f74720b81978d1c18ee06e4dd30
winstonallo <arthurbiedchar@gmail.com>
2025-12-02 11:27:27 +0100

    Add pub fn kfree

--- src/vmm/allocators/kmalloc.rs
+++ src/vmm/allocators/kmalloc.rs
@@ -133,6 +133,11 @@ pub const BUDDY_ALLOCATOR_SIZE: usize = 1 << 29;
 static mut BUDDY_ALLOCATOR: BuddyAllocator = BuddyAllocator::new(core::ptr::null(), BUDDY_ALLOCATOR_SIZE, unsafe { LEVELS });
 
 #[allow(static_mut_refs)]
+pub fn kfree(addr: *const u8) -> Result<(), KfreeError> {
+    unsafe { BUDDY_ALLOCATOR.free(addr) }
+}
+
+#[allow(static_mut_refs)]
 pub fn kmalloc(size: usize) -> Result<*const u8, KmallocError> {
     unsafe { BUDDY_ALLOCATOR.alloc(size).map_err(|_| KmallocError::NotEnoughMemory) }
 }
@@ -147,6 +152,8 @@ pub fn init() -> Result<(), KmallocError> {
         cache_memory
     );
     let mut bm = unsafe { &mut BUDDY_ALLOCATOR };
+
     bm.set_root(cache_memory as *const u8);
+
     Ok(())
 }
