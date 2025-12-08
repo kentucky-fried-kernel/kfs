abccfc853053bb98a13b53215514faac9985317f
winstonallo <arthurbiedchar@gmail.com>
2025-12-08 13:50:05 +0100

    Move more fine-grained data structures to the top of slab.rs

--- src/vmm/allocators/backend/slab.rs
+++ src/vmm/allocators/backend/slab.rs
@@ -8,6 +8,237 @@ use crate::{
     },
 };
 
+#[derive(Debug)]
+pub enum SlabAllocationError {
+    NotEnoughMemory,
+}
+
+#[derive(Debug)]
+pub enum SlabFreeError {
+    InvalidPointer,
+}
+
+#[derive(Clone, Copy, Debug)]
+pub struct Payload {
+    next: Option<NonNull<Payload>>,
+}
+
+pub trait SlabOps: IntrusiveLink + Sized {
+    /// How many contiguous pages this slab spans
+    const ORDER: usize;
+
+    /// Total size of this slab in bytes
+    const SLAB_SIZE: usize = PAGE_SIZE * Self::ORDER;
+
+    /// Initializes a slab in place at the given address.
+    ///
+    /// # Safety
+    /// If any of the following conditions are violated, the result is Undefined
+    /// Behavior:
+    /// * `slab_ptr` must point to a page-aligned allocation of **at least** `0x1000 * Self::ORDER`
+    ///   bytes.
+    ///
+    /// # Panics
+    /// This function will panic if called with wrong arguments, like a
+    /// `slab_ptr` which is not page-aligned.
+    unsafe fn init(slab_ptr: *mut Self, object_size: usize);
+
+    #[must_use]
+    fn address(&self) -> *const u8;
+
+    #[must_use]
+    fn full(&self) -> bool;
+
+    fn max_objects(&self) -> usize;
+
+    fn object_size(&self) -> usize;
+
+    fn allocated(&self) -> usize;
+
+    /// Returns a pointer to a free memory region of size `object_size`, or a
+    /// `SlabAllocationError` if no more space is left.
+    ///
+    /// # Errors
+    /// Returns a `SlabAllocationError` if allocation is not possible due to insufficient memory.
+    fn alloc(&mut self) -> Result<*mut u8, SlabAllocationError>;
+
+    /// # Errors
+    /// Returns a `SlabFreeError` if `addr` does not point to memory managed by this `Slab` object.
+    fn free(&mut self, addr: *const u8) -> Result<(), SlabFreeError>;
+}
+
+/// Generic slab struct managing slabs of memory of size `object_size`. The `ORDER` const generic
+/// allows to specify how many pages one slab should span.
+///
+/// https://github.com/kentucky-fried-kernel/kfs/issues/58
+#[derive(Clone, Copy, Debug)]
+pub struct Slab<const ORDER: usize> {
+    /// Intrusive list link for `SlabCache` lists (empty/partial/full)
+    list_next: Option<NonNull<Slab<ORDER>>>,
+    /// Size of each object in this slab
+    object_size: usize,
+    /// Number of currently allocated objects
+    allocated: usize,
+    /// Free list head - points to the next available object
+    free_list_next: Option<NonNull<Payload>>,
+}
+
+const fn slab_header_overhead<const ORDER: usize>() -> usize {
+    (size_of::<Slab<ORDER>>() & !(0x08 - 1)) + 0x08
+}
+
+impl<const ORDER: usize> IntrusiveLink for Slab<ORDER> {
+    #[inline]
+    fn next_ptr(&self) -> Option<NonNull<Self>> {
+        self.list_next
+    }
+
+    #[inline]
+    fn next_ptr_mut(&mut self) -> &mut Option<NonNull<Self>> {
+        &mut self.list_next
+    }
+}
+
+impl<const ORDER: usize> SlabOps for Slab<ORDER> {
+    const ORDER: usize = ORDER;
+
+    #[inline]
+    fn object_size(&self) -> usize {
+        self.object_size
+    }
+
+    #[inline]
+    fn allocated(&self) -> usize {
+        self.allocated
+    }
+
+    #[inline]
+    fn full(&self) -> bool {
+        self.allocated == self.max_objects()
+    }
+
+    #[inline]
+    fn address(&self) -> *const u8 {
+        (self as *const Self).cast()
+    }
+
+    fn alloc(&mut self) -> Result<*mut u8, SlabAllocationError> {
+        if self.allocated == self.max_objects() {
+            return Err(SlabAllocationError::NotEnoughMemory);
+        }
+
+        let allocation = self.free_list_next.ok_or(SlabAllocationError::NotEnoughMemory)?;
+
+        // SAFETY:
+        // If this `Slab` was intialized according to its safety documentation,
+        // `allocation` is guaranteed to be usable memory that we can safely
+        // access.
+        self.free_list_next = unsafe { *allocation.as_ptr() }.next;
+        self.allocated += 1;
+
+        Ok(allocation.as_ptr().cast())
+    }
+
+    // We cast from `*const u8` to more strictly aligned pointers (`*mut Payload`),
+    // however the assertion in the beginning of the function ensures that no
+    // pointer is passed that is not at least 8-bytes aligned.
+    #[allow(clippy::cast_ptr_alignment)]
+    fn free(&mut self, addr: *const u8) -> Result<(), SlabFreeError> {
+        debug_assert!(addr.is_aligned_to(8));
+
+        let slab_end = (self.address() as usize + Self::SLAB_SIZE) as *const u8;
+        if addr < self.address() || addr > slab_end {
+            return Err(SlabFreeError::InvalidPointer);
+        }
+
+        let next = self.free_list_next;
+        self.free_list_next = NonNull::new(addr as *mut Payload);
+        self.allocated -= 1;
+
+        // SAFETY:
+        // If this `Slab` was intialized according to its safety documentation,
+        // `addr` is guaranteed to be memory owned by this slab that we can safely
+        // access.
+        unsafe { (*(addr as *mut Payload)).next = next };
+
+        Ok(())
+    }
+
+    unsafe fn init(slab_ptr: *mut Self, object_size: usize) {
+        let addr = slab_ptr.cast::<u8>();
+        debug_assert!(addr.is_aligned_to(PAGE_SIZE), "addr is not page-aligned");
+        debug_assert!(object_size >= 8, "object_size must be at least 8");
+
+        let header_overhead = slab_header_overhead::<ORDER>();
+
+        // SAFETY:
+        // * `header_overhead` is a constant that does not overflow `isize`
+        // * According to this function's Safety docs, `addr` must point to a valid allocation that we can
+        //   safely access
+        let objects_start_addr = unsafe { addr.add(header_overhead) };
+
+        let available_space = Self::SLAB_SIZE - header_overhead;
+        let n_objects = available_space / object_size;
+
+        debug_assert!(n_objects > 0, "object_size is too large for order {} slab", ORDER);
+
+        // SAFETY:
+        // According to this function's safety documentation, `slab_ptr` must point
+        // to a valid allocation of at least `0x1000` bytes that we can safely access.
+        #[allow(clippy::multiple_unsafe_ops_per_block)]
+        unsafe {
+            (*slab_ptr).list_next = None;
+            (*slab_ptr).object_size = object_size;
+            (*slab_ptr).allocated = 0;
+        }
+
+        // Initialize the free list
+        let mut current_obj_ptr = objects_start_addr;
+        for i in 0..n_objects {
+            // SAFETY:
+            // According to this function's safety documentation, `slab_ptr` must point
+            // to a valid allocation of at least `0x1000 * Self::ORDER` bytes that we can safely access.
+            // The loop is bounded to `n_objects`, which guarantees that no address after
+            // `slab_ptr + 0x1000 * Self::ORDER` will be accessed.
+            let next_obj_ptr = unsafe { current_obj_ptr.add(object_size) };
+
+            // We are casting `*const u8` to a more strictly aligned pointer
+            // (`*mut *const u8`), however we know that `current_obj_ptr` is
+            // at least 8-bytes aligned due to the restrictions enforced at
+            // the beginning of this function.
+            #[allow(clippy::cast_ptr_alignment)]
+            let link_ptr = current_obj_ptr.cast::<*const u8>();
+            let value = if i == n_objects - 1 { core::ptr::null() } else { next_obj_ptr };
+
+            // SAFETY:
+            // We are dereferencing `link_ptr`, which is guaranteed to be in a valid
+            // allocation by this function's Safety docs and the bounds of this
+            // loop.
+            unsafe { *link_ptr = value };
+
+            current_obj_ptr = next_obj_ptr;
+        }
+
+        // We are casting `*const u8` to a more strictly aligned pointer
+        // (`*mut *const u8`), however we know that `current_obj_ptr` is
+        // at least 8-bytes aligned due to the restrictions enforced at
+        // the beginning of this function.
+        //
+        // SAFETY:
+        // We are dereferencing `slab_ptr`, which is guaranteed to be in a valid
+        // allocation by this function's Safety docs.
+        #[allow(clippy::cast_ptr_alignment)]
+        unsafe {
+            (*slab_ptr).free_list_next = NonNull::new(objects_start_addr.cast());
+        }
+    }
+
+    #[inline]
+    fn max_objects(&self) -> usize {
+        (Self::SLAB_SIZE - slab_header_overhead::<ORDER>()) / self.object_size
+    }
+}
+
 #[derive(Clone, Copy, Debug)]
 pub struct SlabCache<S: SlabOps> {
     empty_slabs: List<S>,
@@ -327,242 +558,5 @@ impl SlabAllocator {
             }
         }
         Err(KfreeError::InvalidPointer)
-    }
-}
-
-#[repr(u8)]
-pub enum SlabObjectStatus {
-    Free = 0,
-    Allocated = 1,
-}
-
-#[derive(Debug)]
-pub enum SlabAllocationError {
-    NotEnoughMemory,
-}
-
-#[derive(Debug)]
-pub enum SlabFreeError {
-    InvalidPointer,
-}
-
-#[derive(Clone, Copy, Debug)]
-pub struct Payload {
-    next: Option<NonNull<Payload>>,
-}
-
-pub trait SlabOps: IntrusiveLink + Sized {
-    /// How many contiguous pages this slab spans
-    const ORDER: usize;
-
-    /// Total size of this slab in bytes
-    const SLAB_SIZE: usize = PAGE_SIZE * Self::ORDER;
-
-    /// Initializes a slab in place at the given address.
-    ///
-    /// # Safety
-    /// If any of the following conditions are violated, the result is Undefined
-    /// Behavior:
-    /// * `slab_ptr` must point to a page-aligned allocation of **at least** `0x1000 * Self::ORDER`
-    ///   bytes.
-    ///
-    /// # Panics
-    /// This function will panic if called with wrong arguments, like a
-    /// `slab_ptr` which is not page-aligned.
-    unsafe fn init(slab_ptr: *mut Self, object_size: usize);
-
-    #[must_use]
-    fn address(&self) -> *const u8;
-
-    #[must_use]
-    fn full(&self) -> bool;
-
-    fn max_objects(&self) -> usize;
-
-    fn object_size(&self) -> usize;
-
-    fn allocated(&self) -> usize;
-
-    /// Returns a pointer to a free memory region of size `object_size`, or a
-    /// `SlabAllocationError` if no more space is left.
-    ///
-    /// # Errors
-    /// Returns a `SlabAllocationError` if allocation is not possible due to insufficient memory.
-    fn alloc(&mut self) -> Result<*mut u8, SlabAllocationError>;
-
-    /// # Errors
-    /// Returns a `SlabFreeError` if `addr` does not point to memory managed by this `Slab` object.
-    fn free(&mut self, addr: *const u8) -> Result<(), SlabFreeError>;
-}
-
-/// Generic slab struct managing slabs of memory of size `object_size`. The `ORDER` const generic
-/// allows to specify how many pages one slab should span.
-///
-/// https://github.com/kentucky-fried-kernel/kfs/issues/58
-#[derive(Clone, Copy, Debug)]
-pub struct Slab<const ORDER: usize> {
-    /// Intrusive list link for `SlabCache` lists (empty/partial/full)
-    list_next: Option<NonNull<Slab<ORDER>>>,
-    /// Size of each object in this slab
-    object_size: usize,
-    /// Number of currently allocated objects
-    allocated: usize,
-    /// Free list head - points to the next available object
-    free_list_next: Option<NonNull<Payload>>,
-}
-
-const fn slab_header_overhead<const ORDER: usize>() -> usize {
-    (size_of::<Slab<ORDER>>() & !(0x08 - 1)) + 0x08
-}
-
-impl<const ORDER: usize> IntrusiveLink for Slab<ORDER> {
-    #[inline]
-    fn next_ptr(&self) -> Option<NonNull<Self>> {
-        self.list_next
-    }
-
-    #[inline]
-    fn next_ptr_mut(&mut self) -> &mut Option<NonNull<Self>> {
-        &mut self.list_next
-    }
-}
-
-impl<const ORDER: usize> SlabOps for Slab<ORDER> {
-    const ORDER: usize = ORDER;
-
-    #[inline]
-    fn object_size(&self) -> usize {
-        self.object_size
-    }
-
-    #[inline]
-    fn allocated(&self) -> usize {
-        self.allocated
-    }
-
-    #[inline]
-    fn full(&self) -> bool {
-        self.allocated == self.max_objects()
-    }
-
-    #[inline]
-    fn address(&self) -> *const u8 {
-        (self as *const Self).cast()
-    }
-
-    fn alloc(&mut self) -> Result<*mut u8, SlabAllocationError> {
-        if self.allocated == self.max_objects() {
-            return Err(SlabAllocationError::NotEnoughMemory);
-        }
-
-        let allocation = self.free_list_next.ok_or(SlabAllocationError::NotEnoughMemory)?;
-
-        // SAFETY:
-        // If this `Slab` was intialized according to its safety documentation,
-        // `allocation` is guaranteed to be usable memory that we can safely
-        // access.
-        self.free_list_next = unsafe { *allocation.as_ptr() }.next;
-        self.allocated += 1;
-
-        Ok(allocation.as_ptr().cast())
-    }
-
-    // We cast from `*const u8` to more strictly aligned pointers (`*mut Payload`),
-    // however the assertion in the beginning of the function ensures that no
-    // pointer is passed that is not at least 8-bytes aligned.
-    #[allow(clippy::cast_ptr_alignment)]
-    fn free(&mut self, addr: *const u8) -> Result<(), SlabFreeError> {
-        debug_assert!(addr.is_aligned_to(8));
-
-        let slab_end = (self.address() as usize + Self::SLAB_SIZE) as *const u8;
-        if addr < self.address() || addr > slab_end {
-            return Err(SlabFreeError::InvalidPointer);
-        }
-
-        let next = self.free_list_next;
-        self.free_list_next = NonNull::new(addr as *mut Payload);
-        self.allocated -= 1;
-
-        // SAFETY:
-        // If this `Slab` was intialized according to its safety documentation,
-        // `addr` is guaranteed to be memory owned by this slab that we can safely
-        // access.
-        unsafe { (*(addr as *mut Payload)).next = next };
-
-        Ok(())
-    }
-
-    unsafe fn init(slab_ptr: *mut Self, object_size: usize) {
-        let addr = slab_ptr.cast::<u8>();
-        debug_assert!(addr.is_aligned_to(PAGE_SIZE), "addr is not page-aligned");
-        debug_assert!(object_size >= 8, "object_size must be at least 8");
-
-        let header_overhead = slab_header_overhead::<ORDER>();
-
-        // SAFETY:
-        // * `header_overhead` is a constant that does not overflow `isize`
-        // * According to this function's Safety docs, `addr` must point to a valid allocation that we can
-        //   safely access
-        let objects_start_addr = unsafe { addr.add(header_overhead) };
-
-        let available_space = Self::SLAB_SIZE - header_overhead;
-        let n_objects = available_space / object_size;
-
-        debug_assert!(n_objects > 0, "object_size is too large for order {} slab", ORDER);
-
-        // SAFETY:
-        // According to this function's safety documentation, `slab_ptr` must point
-        // to a valid allocation of at least `0x1000` bytes that we can safely access.
-        #[allow(clippy::multiple_unsafe_ops_per_block)]
-        unsafe {
-            (*slab_ptr).list_next = None;
-            (*slab_ptr).object_size = object_size;
-            (*slab_ptr).allocated = 0;
-        }
-
-        // Initialize the free list
-        let mut current_obj_ptr = objects_start_addr;
-        for i in 0..n_objects {
-            // SAFETY:
-            // According to this function's safety documentation, `slab_ptr` must point
-            // to a valid allocation of at least `0x1000 * Self::ORDER` bytes that we can safely access.
-            // The loop is bounded to `n_objects`, which guarantees that no address after
-            // `slab_ptr + 0x1000 * Self::ORDER` will be accessed.
-            let next_obj_ptr = unsafe { current_obj_ptr.add(object_size) };
-
-            // We are casting `*const u8` to a more strictly aligned pointer
-            // (`*mut *const u8`), however we know that `current_obj_ptr` is
-            // at least 8-bytes aligned due to the restrictions enforced at
-            // the beginning of this function.
-            #[allow(clippy::cast_ptr_alignment)]
-            let link_ptr = current_obj_ptr.cast::<*const u8>();
-            let value = if i == n_objects - 1 { core::ptr::null() } else { next_obj_ptr };
-
-            // SAFETY:
-            // We are dereferencing `link_ptr`, which is guaranteed to be in a valid
-            // allocation by this function's Safety docs and the bounds of this
-            // loop.
-            unsafe { *link_ptr = value };
-
-            current_obj_ptr = next_obj_ptr;
-        }
-
-        // We are casting `*const u8` to a more strictly aligned pointer
-        // (`*mut *const u8`), however we know that `current_obj_ptr` is
-        // at least 8-bytes aligned due to the restrictions enforced at
-        // the beginning of this function.
-        //
-        // SAFETY:
-        // We are dereferencing `slab_ptr`, which is guaranteed to be in a valid
-        // allocation by this function's Safety docs.
-        #[allow(clippy::cast_ptr_alignment)]
-        unsafe {
-            (*slab_ptr).free_list_next = NonNull::new(objects_start_addr.cast());
-        }
-    }
-
-    #[inline]
-    fn max_objects(&self) -> usize {
-        (Self::SLAB_SIZE - slab_header_overhead::<ORDER>()) / self.object_size
     }
 }
