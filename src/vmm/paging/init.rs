0f0e344d434bdb965d9498c62f7480d44582743c
winstonallo <arthurbiedchar@gmail.com>
2025-12-09 11:55:04 +0100

    Remove unused import

--- src/vmm/paging/init.rs
+++ src/vmm/paging/init.rs
@@ -2,7 +2,6 @@ use core::arch::asm;
 
 use crate::{
     boot::{KERNEL_BASE, MultibootInfo, MultibootMmapEntry},
-    printkln,
     vmm::paging::{
         Access, PAGE_SIZE,
         page_entries::{PageDirectoryEntry, PageTableEntry},
