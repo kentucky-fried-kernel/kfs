646e76b1c2447313c66dd15a7a7611d90af56cf3
winstonallo <arthurbiedchar@gmail.com>
2025-12-09 14:46:49 +0100

    Remove commented out code

--- src/vmm/paging/page_entries.rs
+++ src/vmm/paging/page_entries.rs
@@ -2,67 +2,6 @@ pub const PAGE_DIRECTORY_SIZE: usize = 1024;
 #[repr(align(0x1000))]
 pub struct PageDirectory(pub [PageDirectoryEntry; PAGE_DIRECTORY_SIZE]);
 
-// pub struct  OutOfBoundsError;
-
-// impl PageDirectory {
-//     pub const fn new(entries: &'static mut [PageDirectoryEntry;
-// PAGE_DIRECTORY_SIZE]) -> Self {         Self { entries }
-//     }
-
-//     pub fn set_entry(&mut self, index: usize, entry: PageDirectoryEntry) ->
-// Result<(), OutOfBoundsError> {         if index >= self.entries.len() {
-//             return Err(OutOfBoundsError);
-//         }
-
-//         self.entries[index] = entry;
-
-//         Ok(())
-//     }
-//     pub fn get_entry(&'static self, index: usize) -> Option<&'static mut
-// PageDirectoryEntry> {         if index >= self.entries.len() {
-//             return None;
-//         }
-
-//         Some(&self.entries[index])
-//     }
-//     pub fn get_entry_mut(&mut self, index: usize) -> Option<&mut
-// PageDirectoryEntry> {         if index >= self.entries.len() {
-//             return None;
-//         }
-
-//         Some(&mut self.entries[index])
-//     }
-// }
-
-// pub struct PageDirectoryIntoIterator {
-//     page_directory: &'static mut PageDirectory,
-//     pos: usize,
-// }
-
-// impl Iterator for PageDirectoryIntoIterator {
-//     type Item = &'static mut PageDirectoryEntry;
-
-//     fn next(&mut self) -> Option<Self::Item> {
-//         if self.pos >= self.page_directory.entries.len() {
-//             return None;
-//         }
-
-//         let cur_pos = self.pos;
-//         self.pos += 1;
-//         let p: &'static mut PageDirectoryEntry =
-// self.page_directory.get_entry(cur_pos).unwrap();         Some(p)
-//     }
-// }
-
-// impl IntoIterator for &'static mut PageDirectory {
-//     type Item = &'static mut PageDirectoryEntry;
-//     type IntoIter = PageDirectoryIntoIterator;
-
-//     fn into_iter(self) -> Self::IntoIter {
-//         Self::IntoIter { page_directory: self, pos: 0}
-//     }
-// }
-
 pub const PAGE_TABLE_SIZE: usize = 1024;
 #[repr(align(0x1000))]
 #[derive(Clone, Copy)]
