a82f2700fc64ad5287c15a060647003834d04cfe
fbruggem <fbruggem@proton.me>
2025-12-13 13:45:01 +0100

    what

---
+++ src/shell.rs
@@ -0,0 +1,158 @@
+use crate::{
+    ps2::{self, Key},
+    qemu::{ExitCode, exit},
+    serial, serial_print, serial_println,
+    terminal::{self, Screen, entry::Entry, screen, vga::Buffer},
+};
+
+type Character = u8;
+
+pub struct Shell<'a> {
+    screen: &'a mut Screen,
+    prompt: Prompt,
+}
+
+impl<'a> Shell<'a> {
+    pub fn default(screen: &'a mut Screen) -> Self {
+        Self {
+            screen,
+            prompt: Prompt::default(),
+        }
+    }
+    pub fn launch(&mut self) {
+        loop {
+            self.screen.write("sh> ");
+            self.flush();
+
+            loop {
+                if let Some(key) = ps2::read_if_ready() {
+                    match key {
+                        Key::Enter => {
+                            self.screen.push(Entry::new(b'\n'));
+                            for c in self.prompt.entries.iter().take(self.prompt.len) {
+                                serial_print!("{:?} ", *c as char);
+                            }
+                            serial_println!("");
+                            let _ = self.prompt.execute(self.screen);
+                            self.prompt.clear();
+                            break;
+                        }
+                        Key::Backspace => {
+                            if self.prompt.len != 0 {
+                                self.screen.remove_last();
+                                self.prompt.len -= 1;
+                            }
+                        }
+                        Key::ArrowUp => {}
+                        _ => {
+                            let _ = Self::push(self, key as u8);
+                        }
+                    };
+                    self.flush();
+                }
+            }
+        }
+    }
+
+    fn push(&mut self, c: Character) -> Result<(), PromptPushError> {
+        self.prompt.push(c)?;
+        self.screen.push(Entry::new(c));
+        Ok(())
+    }
+
+    pub fn flush(&mut self) {
+        let b = Buffer::from_screen(self.screen);
+        b.flush();
+    }
+}
+
+// This is to validate that the prompt is never bigger than the screen size
+const PROMT_SIZE: usize = terminal::screen::BUFFER_SIZE - 0x1000;
+
+#[derive(Debug)]
+pub struct Prompt {
+    entries: [Character; PROMT_SIZE],
+    len: usize,
+}
+
+impl Prompt {
+    pub fn default() -> Self {
+        Self {
+            entries: [0; PROMT_SIZE],
+            len: 0,
+        }
+    }
+
+    pub fn execute(&self, screen: &mut Screen) -> Result<(), ()> {
+        let prompt = &self.entries[..self.len];
+
+        for command in COMMANDS {
+            serial_println!("hello");
+            let is_the_same_string = &prompt[..command.name.len()] == command.name.as_bytes();
+            let has_trailing_space_or_same_size = prompt[command.name.len()] == b' ' || command.name.len() == self.len;
+            if is_the_same_string && has_trailing_space_or_same_size {
+                let args = &prompt[command.name.len() + 1..];
+                (command.func)(args, screen);
+                return Ok(());
+            }
+        }
+        Ok(())
+    }
+
+    pub fn push(&mut self, c: Character) -> Result<(), PromptPushError> {
+        if self.len >= self.entries.len() {
+            Err(PromptPushError::PromptFull)
+        } else {
+            self.entries[self.len] = c;
+            self.len += 1;
+            Ok(())
+        }
+    }
+
+    pub fn clear(&mut self) {
+        self.len = 0;
+    }
+}
+
+pub enum PromptPushError {
+    PromptFull,
+}
+struct Command<'a> {
+    name: &'a str,
+    func: fn(args: &[u8], s: &mut Screen),
+}
+const COMMANDS: &[Command] = &[
+    Command { name: "echo", func: echo_cmd },
+    // Command {
+    //     name: "panic",
+    //     func: panic_cmd,
+    // },
+    // Command { name: "halt", func: halt_cmd },
+    // Command {
+    //     name: "reboot",
+    //     func: reboot_cmd,
+    // },
+    // Command {
+    //     name: "prints",
+    //     func: prints_cmd,
+    // },
+    // Command { name: "help", func: help_cmd },
+    // Command {
+    //     name: "printsb",
+    //     func: printsb_cmd,
+    // },
+    Command { name: "exit", func: exit_cmd },
+];
+
+fn echo_cmd(args: &[u8], s: &mut Screen) {
+    for c in args.iter() {
+        s.push(Entry::new(*c));
+    }
+    s.push(Entry::new(b'\n'));
+}
+
+// TODO: doesn't correctly exit
+fn exit_cmd(_: &[u8], _: &mut Screen) {
+    serial_println!("exited");
+    unsafe { exit(ExitCode::Success) };
+}
---
+++ src/terminal/entry.rs
@@ -0,0 +1,78 @@
+/// Represents a single character entry for the Screen buffer.
+///
+/// Each `Entry` consists of a character and a color attribute. The color is set
+/// to the default color (light gray on black) by default, but it can be
+/// customized. Each `Entry` can be converted into a `u16` value, which is the
+/// format used for writing to the VGA buffer.
+#[derive(Clone, Copy, Debug)]
+pub struct Entry {
+    color: u8,
+    character: u8,
+}
+
+impl Entry {
+    /// Creates a new `Entry` with the specified character and the default
+    /// color.
+    ///
+    /// The default color is light gray (`0x07`).
+    ///
+    /// ### Parameters:
+    /// - `character`: The character to be storedy.
+    #[must_use]
+    pub const fn new(character: u8) -> Self {
+        Entry {
+            color: Color::Default as u8,
+            character,
+        }
+    }
+
+    /// Creates a new `Entry` with the specified character and custom color.
+    ///
+    /// This function allows the creation of a `Entry` with a specific character
+    /// and color, where the color is passed as a parameter. The color is
+    /// represented as an 8-bit value, allowing for a wide range of color
+    /// codes (e.g., for screen colors). The character is displayed with
+    /// this color when rendered to the VGA buffer.
+    ///
+    /// ### Parameters:
+    /// - `character`: The character to be displayed (e.g., an ASCII value representing a letter or
+    ///   symbol).
+    /// - `color`: The color code for the character (an 8-bit value that determines the character's
+    ///   color).
+    ///   - The value should correspond to a color in the VGA color palette (for example, `0x0F` for
+    ///     white, `0x01` for blue, etc.).
+    #[must_use]
+    pub fn new_with_color(character: u8, color: u8) -> Self {
+        Entry { color, character }
+    }
+
+    /// Converts this `Entry` into a `u16` value that can be written to the VGA
+    /// buffer.
+    ///
+    /// The `u16` format stores the color in the upper 8 bits and the character
+    /// in the lower 8 bits.
+    ///
+    /// ### Returns:
+    /// A `u16` value representing this `Entry`.
+    #[must_use]
+    pub const fn to_u16(&self) -> u16 {
+        ((self.color as u16) << 8) | (self.character as u16)
+    }
+
+    pub const fn get_character(&self) -> u8 {
+        self.character
+    }
+}
+
+/// Represents the available color codes for screen entries.
+///
+/// The colors are defined as `u8` values, where each value corresponds to a
+/// particular color. The default color is light gray on black.
+#[repr(u8)]
+pub enum Color {
+    /// Light gray on black (default)
+    Default = 0x07,
+    /// White on Red
+    #[allow(unused)]
+    Error = 0x4F,
+}
