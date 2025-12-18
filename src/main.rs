e5abc68ca7d69fa872c001a9f390e2d5a84682e2
winstonallo <arthurbiedchar@gmail.com>
2025-12-18 09:39:10 +0100

    Move imports to kmain scope

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@
 #![test_runner(kfs::tester::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
-use kfs::{boot::MultibootInfo, shell::Shell, terminal::SCREEN};
+use kfs::boot::MultibootInfo;
 
 mod panic;
 
@@ -17,20 +17,24 @@ extern crate alloc;
 pub extern "C" fn kmain(_magic: usize, info: &MultibootInfo) {
     use kfs::{
         arch,
+        shell::Shell,
         vmm::{self, paging::init::init_memory},
     };
 
     arch::x86::gdt::init();
     arch::x86::idt::init();
 
-    // init_memory(info);
+    init_memory(info);
 
-    // if vmm::allocators::kmalloc::init().is_err() {
-    //     panic!("Failed to initialize kmalloc");
-    // }
+    if vmm::allocators::kmalloc::init().is_err() {
+        panic!("Failed to initialize kmalloc");
+    }
 
     #[allow(static_mut_refs)]
-    let mut shell = Shell::default(unsafe { &mut SCREEN });
+    let mut shell = Shell::default(unsafe {
+        use kfs::terminal::SCREEN;
+        &mut SCREEN
+    });
     shell.launch();
 }
 
