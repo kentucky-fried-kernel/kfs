0bdf30a3eef9f533939d82d96e94397284c1f36b
fbruggem <fbruggem@proton.me>
2026-02-13 11:42:37 +0100

    add clear registers macro

--- src/panic.rs
+++ src/panic.rs
@@ -6,7 +6,7 @@ use kfs::terminal::entry::Color;
 #[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    use kfs::{cli, printkln, serial_println};
+    use kfs::{cli, hlt, printkln, serial_println};
 
     cli!();
 
@@ -14,7 +14,31 @@ fn panic(info: &PanicInfo) -> ! {
 
     serial_println!("KERNEL PANIC: {:?}", info.message());
 
-    loop {}
+    clear_regs!();
+    hlt!();
+}
+
+#[macro_export]
+macro_rules! clear_regs {
+    () => {
+        core::arch::asm!(
+            "xor rax, rax",
+            "xor rbx, rbx",
+            "xor rcx, rcx",
+            "xor rdx, rdx",
+            "xor rsi, rsi",
+            "xor rdi, rdi",
+            "xor r8, r8",
+            "xor r9, r9",
+            "xor r10, r10",
+            "xor r11, r11",
+            "xor r12, r12",
+            "xor r13, r13",
+            "xor r14, r14",
+            "xor r15, r15",
+            options(nomem, preserves_flags)
+        );
+    };
 }
 
 #[cfg(test)]
