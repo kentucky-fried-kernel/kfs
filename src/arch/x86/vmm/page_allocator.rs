ff27dc80437a5d647a1e6df27d02d7691472458b
fbruggem <fbruggem@proton.me>
2026-04-17 10:54:59 +0200

    tested and fixed alloc bugs

--- src/arch/x86/vmm/page_allocator.rs
+++ src/arch/x86/vmm/page_allocator.rs
@@ -73,6 +73,7 @@ impl<'a> PageAllocator<'a> {
 
         let smallest_order = smallest_order?;
 
+        // Block already exists in the requested order
         if let Some(next) = self.orders_head[smallest_order] {
             self.node_remove(next, smallest_order);
 
@@ -81,6 +82,7 @@ impl<'a> PageAllocator<'a> {
             return Some(res as *mut u8);
         }
 
+        // It searches if higher order blocks can be broken up for the requestsed order
         if smallest_order + 1 < ORDERS {
             self.split(smallest_order + 1);
         }
@@ -92,8 +94,31 @@ impl<'a> PageAllocator<'a> {
 
             return Some(res as *mut u8);
         }
-        // split
 
+        // Try to coalesce blocks to build requested order from smaller order blocks
+        self.coalesce();
+
+        // Block already exists in the requested order
+        if let Some(next) = self.orders_head[smallest_order] {
+            self.node_remove(next, smallest_order);
+
+            let res = next * pow2(smallest_order) * PAGE_SIZE;
+
+            return Some(res as *mut u8);
+        }
+
+        // It searches if higher order blocks can be broken up for the requestsed order
+        if smallest_order + 1 < ORDERS {
+            self.split(smallest_order + 1);
+        }
+
+        if let Some(next) = self.orders_head[smallest_order] {
+            self.node_remove(next, smallest_order);
+
+            let res = next * pow2(smallest_order) * PAGE_SIZE;
+
+            return Some(res as *mut u8);
+        }
         None
     }
 
@@ -118,9 +143,11 @@ impl<'a> PageAllocator<'a> {
 
         let smallest_order = smallest_order?;
 
-        // The top node has to be done manually because calculating the entry size would perfectly
-        // overflow into 0 again.
-        self.split_node(0, ORDERS - 1);
+        if smallest_order + 1 < ORDERS {
+            // The top node has to be done manually because calculating the entry size would perfectly
+            // overflow into 0 again.
+            self.split_node(0, ORDERS - 1);
+        }
 
         for o in (smallest_order + 1..(ORDERS - 1)).rev() {
             let entry_size = pow2(o) * PAGE_SIZE;
@@ -137,13 +164,35 @@ impl<'a> PageAllocator<'a> {
 
         None
     }
-
+    pub fn print(&self) {
+        for order in (0..ORDERS).rev() {
+            let mut current = self.orders_head[order];
+            while let Some(index) = current {
+                let addr = index * pow2(order) * PAGE_SIZE;
+                let size = pow2(order) * PAGE_SIZE;
+                serial_println!(
+                    "order {:2} | node {:6} | addr 0x{:08x} | size 0x{:x} ({:x} pages)",
+                    order,
+                    index,
+                    addr,
+                    size,
+                    pow2(order)
+                );
+                let node = self.orders[order][index].unwrap();
+                current = node.next();
+            }
+        }
+    }
     pub fn dealloc(&mut self, ptr: *mut u8, size: usize) {
         let mut order = None;
 
         for o in 0..ORDERS {
             let order_size = pow2(o);
-            let mut pages = (size + ptr as usize % (order_size * PAGE_SIZE)) / PAGE_SIZE;
+            if o == ORDERS - 1 {
+                self.node_add(0, o);
+                return;
+            }
+            let mut pages = (size as u64 + ptr as u64 % (order_size as u64 * PAGE_SIZE as u64)) as usize / PAGE_SIZE;
             if size % PAGE_SIZE != 0 {
                 pages += 1;
             }
